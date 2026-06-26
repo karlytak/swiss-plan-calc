2cbccb707883e93718906d777096a019d384d95f
Swiss-broker <support@swissbrokerpro.ch>
2026-06-26 12:50:05 +0200

    fix: feedback button centered bottom

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -54,7 +54,7 @@ function AppShell() {
           <Outlet />
           <AiChat />
         </main>
-        <div className="fixed bottom-6 right-6 z-50">
+        <div className="fixed bottom-6 left-1/2 -translate-x-1/2 z-50">
           <FeedbackWidget />
         </div>
       </div>
